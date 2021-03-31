f21723fda530e2ad02c72efa809b5215b6a89719
Herik Martins <herikcosmo.m@gmail.com>
2021-03-31 16:10:42 -0300

    automatic function

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
 from twitter import Twitter
-
+import keyboard
+from time import sleep
 twitter = Twitter()
-twitter.search_tweet('#herikbot')
+
+
+
+
+def menu():
+    print('='*10)
+    print('BEM VINDO AO HERIK BOT')
+    print('='*10)
+    print('Escolha uma opção:')
+    print('1. Tweetar\n2. Pesquisar Tweets\n3. Responder Tweet\n4. Automatico')
+    opcao = int(input())
+    funcoes(opcao)
+
+def funcoes(opcao):
+    if(opcao == 1):
+        twitter.tweet(input('Tweet: '))
+    elif(opcao == 2):
+        twitter.search_tweet(input('Search Tweet: '))
+    elif(opcao == 3):
+        twitter.reply_tweet(input('Search tweet to reply: '))
+    elif(opcao == 4):
+        print('Modo Automático ativado. Pressione "E" para desativar')
+        while True:
+            try:
+                twitter.reply_tweet_with_image('#herikbot', 'teste.jpeg')
+                
+                if keyboard.is_pressed('e'):
+                    break
+                
+            except: 
+                break
+            
+        
+menu()
+#twitter.reply_tweet_with_image('#herikbot', 'teste.jpeg')
+#twitter.image('teste.jpeg')
+# while True:
+#     twitter.reply_tweet_with_image('#herikbot', 'teste.jpeg')
+#     sleep(5)
     
--- twitter.py
+++ twitter.py
@@ -1,15 +1,18 @@
 from auth import Auth
+from time import sleep
 import tweepy
-from pprint import pprint
-import json
+
 
 class Twitter:
     def __init__(self):
         self.client = Auth().api
     
     def tweet(self, tweet_text):
-        request = self.client.update_status(tweet_text)
-        print('Tweet: '+request)
+        try: 
+            request = self.client.update_status(tweet_text)
+            print('Tweet send: '+tweet_text)
+        except tweepy.TweepError as e:
+            print('Error: '+str(e.message))
     
     def search_tweet(self, search_text):
         try:
@@ -20,5 +23,53 @@ class Twitter:
 
         except tweepy.TweepError as e:
             print('Error: '+str(e.message))
+
+    def reply_tweet(self, search_text):
+        try:
+            request = self.client.search(search_text)
+            for tweet in request:
+                screen_name = tweet.user.screen_name
+                id_status = tweet.id
+                reply = f'@{screen_name} {search_text}'
+                self.client.update_status(reply, id_status)
+        except tweepy.TweepError as e:
+            print('Error: '+str(e.message))
+
+    def reply_tweet_with_image(self, search_text, image):
+        try:
+            print('pesquisando...')
+            request = self.client.search(search_text)
+            ids = open('ids.txt', 'r')
+            ids_id = ids.readlines()
+            
+            ids.close()
+
+            for tweet in request:
+                idtxt = tweet.id_str+'\n'
+                if idtxt not in ids_id:
+                    screen_name = tweet.user.screen_name
+                    id_status = tweet.id
+                    id_image = self.image(image)
+                    reply = f'@{screen_name}'
+                    id_write = open('ids.txt', 'a')
+                    id_write.write(str(id_status)+'\n')
+                    id_write.close()
+                    sleep(1)
+                    self.client.update_status(reply, id_status, media_ids=[id_image])
+        
+        except tweepy.TweepError as e:
+            print('Error: '+str(e.message))
+
+        sleep(5)
+
+
+    def image(self, image):
+        try:
+            request = self.client.media_upload(image)
+            return request.media_id
+        except tweepy.TweepError as e:
+            print('Error: '+str(e.message))
+
+
 
         
